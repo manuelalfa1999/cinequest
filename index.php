2da85773111e177d67dd190cabc040f553821dca
manuelalfa1999 <manuelalfa1999@gmail.com>
2026-05-13 12:35:32 +0200

    cambio nombre algunos archivos

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ if (session_status() === PHP_SESSION_NONE) session_start();
 if (isset($_SESSION['usuario_id'])) {
     header('Location: pages/home.php');
 } else {
-    header('Location: pages/login.php');
+    header('Location: pages/presentacion.php');
 }
 exit;
